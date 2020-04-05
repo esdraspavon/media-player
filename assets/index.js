c74f017ba1d6c842dfa7e5206bfc61e013609be7
Esdras Pavon <esdras.pavon@devlights.com>
2020-04-05 12:52:56 -0300

    intersection observer applied

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,16 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector("video");
 const button = document.querySelector("#play");
 const mute = document.querySelector("#mute");
 
 
-const player = new MediaPlayer({ el: video, plugins: [
-    new AutoPlay()
-] });
+const player = new MediaPlayer({
+    el: video,
+    plugins: [ new AutoPlay(), new AutoPause()],
+});
 
 button.onclick= () => player.paused ? player.play() : player.pause();
 
